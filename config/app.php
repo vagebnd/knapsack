f0fbdbb5dfaff0a5637ac341232492c07d447610
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-05 17:58:11 +0200

    Added descriptions and editor to app config

--- config/app.php
+++ config/app.php
@@ -1,5 +1,25 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value will be used to
+    | prefix specific items related to this theme like metaboxes, options,
+    |
+    */
     'name' => 'knapsack',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Editor
+    |--------------------------------------------------------------------------
+    |
+    | This value will be used to determine which editor should be used to open
+    | editor links. Supported values are: vscode, phpstorm, atom, sublime.
+    |
+    */
+    'editor' => 'vscode',
 ];
