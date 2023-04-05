c6ff1829929920e780dd38918150f466dd36f5cc
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-05 20:06:52 +0200

    added php-cs-fixer

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,10 @@
 const micromatch = require('micromatch')
 
 module.exports = {
-  '*.php': (files) => ['./vendor/bin/phpstan analyse --memory-limit=2G'],
+  '*.php': (files) => [
+    `./vendor/bin/php-cs-fixer fix --config .php-cs-fixer.php ${files.join(' ')}`,
+    './vendor/bin/phpstan analyse --memory-limit=2G',
+  ],
   'composer.json': () => 'composer validate --no-check-publish --strict',
   'resources/**/*': (files) => {
     const match = micromatch(files, ['**/*.{css,js,ts,vue,yaml,html}'], {})
