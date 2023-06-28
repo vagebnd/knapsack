d29904d7f1baaa0ef06f958d47282fbab64aef65
Gianluca Riggio <luca@maximumawesome.nl>
2023-06-28 13:42:38 +0200

    feat: added post view

---
+++ resources/views/components/post/content.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>{{ the_title() }}</h1>
+    <p>{{ the_content() }}</p>
+    {!! $slot !!}
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,18 @@
     <main class="h-screen w-screen bg-slate-900 text-slate-100">
         <section class="container p-24 mx-auto">
             <h1 class="text-4xl">Getting started</h1>
-            <div class="flex gap-2 items-center">
-                <p class="my-3">You can edit this page in</p>
+            <p class="my-3">You can edit this page in</p>
 
-                <a href="{{ $editorUrl }}"
-                    class="inline-flex h-6 rounded-full bg-gradient-to-r from-sky-400/30 via-sky-400 to-sky-400/30 p-px font-medium text-sky-300">
-                    <div class="flex items-center rounded-full px-2.5 bg-slate-800">resources/views/index.blade.php</div>
-                </a>
+            <a href="{{ $editorUrl }}"
+                class="inline-flex h-6 rounded-full bg-gradient-to-r from-sky-400/30 via-sky-400 to-sky-400/30 p-px font-medium text-sky-300">
+                <div class="flex items-center rounded-full px-2.5 bg-slate-800">resources/views/index.blade.php</div>
+            </a>
+
+            <div class="flex gap-2 items-center mt-10">
+                @loop
+                    @component('components.post.content')
+                    @endcomponent
+                @endloop
             </div>
         </section>
     </main>
